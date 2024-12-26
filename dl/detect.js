bf371e1be884c67b24a562761f95d15922005b80
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-12-26 12:17:31 +0100

    oopdat

--- dl/detect.js
+++ dl/detect.js
@@ -2,7 +2,7 @@ const win = "https://cdn-dinde.141412.xyz/DindeGDPS.exe";
 const apk = "https://dl.dindegmdps.us.to/mobile?platform=1";
 const ios = "https://dl.dindegmdps.us.to/mobile?platform=2";
 const idk = "https://dl.dindegmdps.us.to/dl";
-const mac = "https://dimisaio1-my.sharepoint.com/personal/dimisaio_141412_xyz/_layouts/15/download.aspx?share=ESsJ5H8ojcVPllgksCRrJiUBsIE8paApVAIhikOF-oHFaw";
+const mac = "https://dimisaio1-my.sharepoint.com/:f:/g/personal/dimisaio_141412_xyz/EqPBHhvUHVFFtbV3cd_aqlcBp-VlQtD21gT1_2NmjjFLXQ?e=hK2geU";
 let url, pl;
 const ua = platform.os.family;
 
@@ -11,7 +11,7 @@ if (ua.startsWith("Windows")) {
     document.getElementById("tip").innerHTML = "Windows 10 or newer, you can also run \"winget install dindegdps\" on CMD!";
 } else if (ua.toLowerCase().startsWith("os x") || ua.toLowerCase().startsWith("mac")) {
     url = mac; pl = "MacOS";
-    document.getElementById("tip").innerHTML = "After install, run \"sudo xattr -rd com.apple.quarantine /Applications/DindeGDPS.app/\" on the terminal";
+    document.getElementById("tip").innerHTML = "After install, open the TXT file under the \"Applications shortcut\" and follow the instructions";
 } else if (ua == "iOS") {
     url = ios; pl = "iOS";
     document.getElementById("tip").innerHTML = "After install, go to Settings => General => Device Mgmt. and trust the \"Enterprise\"";
