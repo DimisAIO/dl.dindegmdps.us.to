1e2331078a6f4e32d4bfa94bd776affa82542fcf
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-05-27 18:47:34 +0200

    no more direct download 'n stuff

--- dinde.js
+++ dinde.js
@@ -1,5 +1,5 @@
 let x = 0;
-const phr = ["1.9 => 2.2 GDPS", "Supoprts any known device", "iOS Direct Install", "Awesome PC Launcher", "1K+ levels!", "<a href='https://www.demirramon.com/gen/undertale_text_box.png?text=the%20heck%20is%20a%20clubette&box=undertale&character=undertale-undyne&expression=annoyed&font=determination&size=2&t=1713647262' class='noex' target='_blank' referrer='noreferrer noopener'>Undertale OST Levels</a>", "Join us!"];
+const phr = ["1.9 => 2.2 GDPS", "Supports any known device", "iOS Support", "Windows Phone Support", "Awesome PC Launcher", "2.5K+ levels!", "<a href='https://www.demirramon.com/gen/undertale_text_box.png?text=the%20heck%20is%20a%20clubette&box=undertale&character=undertale-undyne&expression=annoyed&font=determination&size=2&t=1713647262' class='noex' target='_blank' referrer='noreferrer noopener'>Undertale OST Levels</a>", "Join us!"];
 function test() {
     document.getElementById("text").innerHTML = phr[x];
     x++
