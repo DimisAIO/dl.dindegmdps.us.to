9d48cc998ac1a20b7410c914dba2d1f3ebd0e401
Jean (masckmaster2007) <63162857+masckmaster2007@users.noreply.github.com>
2026-05-13 12:29:04 +0200

    OKAY A LOT OF CHANGES SCREW YANDERE TYPE SHI CODE

--- dl/detect.js
+++ dl/detect.js
@@ -1,43 +1,55 @@
-const win = "https://github.com/DimisAIO/DindeGDPS-Launcher/releases/latest";
-const winp = "https://gdps.dimisaio.be/w";
-const apk = "https://dl.dindegmdps.us.to/mobile?platform=1";
-const ios = "https://dl.dindegmdps.us.to/mobile?platform=2";
-const idk = "https://dl.dindegmdps.us.to/dl";
-const mac = "https://rytlock.141412.xyz/share/ElT_zZll";
-let url, pl;
-const ua = platform.os.family;
+var downloads = {
+    win: {
+        "name": "Windows",
+        "link": "https://github.com/DimisAIO/DindeGDPS-Launcher/releases/latest",
+        "tip": "Windows 10 or newer, you can also run \"winget install dindegdps\" on CMD!"
+    },
+    winp: {
+        "name": "Windows Phone",
+        "link": "https://gdps.dimisaio.be/w",
+        "tip": "Your phone must be Interop Unlocked and/or have CMD Injector installed!"
+    },
+    linux: {
+        "name": "Linux",
+        "link": "https://github.com/DimisAIO/DindeGDPS-Launcher/releases/latest",
+        "tip": 'On linux distros, use Wine/Proton. Once the Windows installer creates a shortcut, go to its properties and set <b>WINEDLLOVERRIDES="XInput1_4,XInput9_1_0.dll=n,b"</b> as environment variable (Tested on KDE Plasma, Ubuntu)'
+    },
+    macos: {
+        "name": "MacOS",
+        "link": "https://rytlock.141412.xyz/share/ElT_zZll",
+        "tip": "MacOS will quarantine DindeGDPS. Read the instructions on the TXT file to unblock!"
+    },
+    android: {
+        "name": "Android",
+        "link": "/mobile?platform=1",
+    },
+    ios: {
+        "name": "iOS",
+        "link": "/mobile?platform=2",
+    }
+}
 
-if(ua == "Windows Phone") {
-    url = winp; pl = "Windows Phone";
-    document.getElementById("tip").innerHTML = "Only install if you know what you're doing! (do you know what an interop unlock is?)";
-} else if (ua.startsWith("Windows")) {
-    document.getElementById("demo").style.display = "block";
-    url = win; pl = "Windows";
-    document.getElementById("tip").innerHTML = "Windows 10 or newer, you can also run \"winget install dindegdps\" on CMD!";
-} else if (ua.startsWith("Linux")) {
-    document.getElementById("demo").style.display = "block";
-    url = win; pl = "Linux";
-    document.getElementById("tip").innerHTML = 'On linux distros, use Wine/Proton. Once the Windows installer creates a shortcut, go to its properties and set <b>WINEDLLOVERRIDES="XInput1_4,XInput9_1_0.dll=n,b"</b> as environment variable (Tested on KDE Plasma, Ubuntu)'
-} else if (ua.toLowerCase().startsWith("os x") || ua.toLowerCase().startsWith("mac")) {
-    document.getElementById("demo").style.display = "block";
-    url = mac; pl = "MacOS";
-    document.getElementById("tip").innerHTML = "After install, read the TXT file that was alongside the DMG files";
-} else if (ua == "iOS") {
-    url = ios; pl = "iOS";
-} else if (ua == "Android") {
-    url = apk; pl = "Android";
-} else {
-    pl = false;
+var pl;
+switch(platform.os.family) {
+    case "Windows": pl = downloads.win; break;
+    case "Linux": pl = downloads.linux; break;
+    case "Windows Phone": pl = downloads.winp; break;
+    case "OS X": pl = downloads.macos; break;
+    case "Android": pl = downloads.android; break;
+    case "iOS": pl = downloads.ios; break;
 }
 
 if(pl) {
-    document.getElementById("dl").innerText = `Download for ${pl}`;
-    if(pl == "MacOS") dl.innerHTML += " <a href='javascript:goMac()'>(iPad?)</a>";
-    document.getElementById("dl").href = url;
+    document.getElementById("wrongos").style.display = "block";
+    document.getElementById("dl").innerText = `Download for: ${pl.name}!`;
+    if(pl.name == "MacOS") wrongos.innerHTML += " <span id=ioswitch>Using an iPad? <a class=noex href='javascript:goMac()'>Switch to iOS</a></span>";
+    document.getElementById("dl").href = pl.link;
+    if(pl.tip) document.getElementById("tip").innerHTML = pl.tip;
 }
 
 function goMac() {
-    document.getElementById("dl").innerText = `Download for iOS`;
-    document.getElementById("dl").href = ios;
+    document.getElementById("ioswitch").style.display = "none";
+    document.getElementById("dl").innerText = `Download for: iOS!`;
+    document.getElementById("dl").href = downloads.ios.link;
     document.getElementById("tip").innerHTML = "";
 }
